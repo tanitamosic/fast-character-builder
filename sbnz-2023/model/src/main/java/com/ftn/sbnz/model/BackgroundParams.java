f45b43050bdcc91de9dd516bf2ad607e3b25458e
Mihajlo Djordjevic <mihajlodjordjevic931@gmail.com>
2023-06-13 00:18:57 +0200

    Frontend gotov. Dodat backward chaining drl na backu.

--- sbnz-2023/model/src/main/java/com/ftn/sbnz/model/BackgroundParams.java
+++ sbnz-2023/model/src/main/java/com/ftn/sbnz/model/BackgroundParams.java
@@ -7,4 +7,35 @@ public class BackgroundParams {
     public PathInLife path;
     public Integer worldly;
 
+    public WayOfLife getWayOfLife() {
+        return wayOfLife;
+    }
+
+    public void setWayOfLife(WayOfLife wayOfLife) {
+        this.wayOfLife = wayOfLife;
+    }
+
+    public Interest getInterest() {
+        return interest;
+    }
+
+    public void setInterest(Interest interest) {
+        this.interest = interest;
+    }
+
+    public PathInLife getPath() {
+        return path;
+    }
+
+    public void setPath(PathInLife path) {
+        this.path = path;
+    }
+
+    public Integer getWorldly() {
+        return worldly;
+    }
+
+    public void setWorldly(Integer worldly) {
+        this.worldly = worldly;
+    }
 }
